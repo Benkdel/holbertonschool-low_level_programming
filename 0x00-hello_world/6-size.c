8b5d62452d880e7347addede374b5da91869791e
Carlos Bentin <3669@holbertonschool.com>
2021-09-30 17:09:10 +0000

    fix: typo and return 0 missing

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,4 +12,5 @@ int main(void)
 	printf("%s %d %s\n", "Size of a long int:", sizeof(long int), "byte(s)");
 	printf("%s %d %s\n", "Size of a long long int:", sizeof(long long int), "byte(s)");
 	printf("%s %d %s\n", "Size of a float:", sizeof(float), "byte(s)");
+	return (0);
 }
