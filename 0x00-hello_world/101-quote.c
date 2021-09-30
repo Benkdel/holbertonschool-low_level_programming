2da5a2ce218d492f079817c3d48c46254bd9c8f3
Carlos Bentin <3669@holbertonschool.com>
2021-09-30 19:37:34 +0000

    fix script 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,9 +7,9 @@
  */
 int main(void)
 {
-	char err[59] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char err[65] = "and that piece of art is useful\" - Dora Korpar, 2015-10";
 
 	fwrite(err, 57, 1, stderr);
-	fwrite("\n", 2, 1, stderr);
+	fwrite("-19\n", 5, 1, stderr);
 	return (1);
 }
