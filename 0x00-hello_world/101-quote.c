c9f95eaabe971c7f22f0eeb64972a942508baf98
Carlos Bentin <3669@holbertonschool.com>
2021-09-30 19:47:12 +0000

    fix inc output

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,10 +8,10 @@
  */
 int main(void)
 {
-	char err[65] = "and that piece of art is useful\" - Dora Korpar, 2015-10";
+	char err[59] = "and that piece of art is useful\" - Dora Korpar, 2015-1";
 
 	fwrite(err, 57, 1, stderr);
-	fwrite("-19\n", 5, 1, stderr);
+	fwrite("0-19\n", 6, 1, stderr);
 	exit(EXIT_FAILURE);
 	return (0);
 }
