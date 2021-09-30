f2a041bbc085248ba1c62214d4ad1a6c8b876647
Carlos Bentin <3669@holbertonschool.com>
2021-09-30 17:08:10 +0000

    fix script 6: typo in last printf func

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,5 +11,5 @@ int main(void)
 	printf("%s %d %s\n", "Size of an int:", sizeof(int), "byte(s)");
 	printf("%s %d %s\n", "Size of a long int:", sizeof(long int), "byte(s)");
 	printf("%s %d %s\n", "Size of a long long int:", sizeof(long long int), "byte(s)");
-	printf("%s %d %s\n", "Size of a float:", sizeof(float), "bytes(s)");
+	printf("%s %d %s\n", "Size of a float:", sizeof(float), "byte(s)");
 }
