184bd4f92688cb3efec0ae994a4cec3c01efd573
Carlos Bentin <3669@holbertonschool.com>
2021-10-08 03:05:53 +0000

    fix: task 3

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,41 +11,17 @@
  */
 void print_number(int n)
 {
-	int nDigits = -1;
-	int reverseNumber = 0;
-	int m = n;
-	int decimalPos = 1;
-	int i, isneg = 0;
+	int r;
 
 	if (n < 0)
 	{
 		n *= -1;
-		isneg = 1;
+		_putchar(45);
 	}
 	if (n == 0)
-		_putchar('0');
-	while (m != 0)
-	{
-		m /= 10;
-		nDigits++;
-	}
-	while (n != 0)
-	{
-		for (i = 0; i < nDigits; i++)
-			decimalPos *= 10;
-		nDigits--;
-		reverseNumber += (n % 10) * decimalPos;
-		n /= 10;
-		decimalPos = 1;
-	}
-	while (reverseNumber != 0)
-	{
-		if (isneg == 1)
-		{
-			_putchar(45);
-			isneg = 0;
-		}
-		_putchar((reverseNumber % 10) + '0');
-		reverseNumber /= 10;
-	}
+		return;
+	r = n % 10;
+
+	print_number(n / 10);
+	_putchar(r + '0');
 }
