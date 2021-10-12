5675698f15f1f03e0ab8661fdb95ba5bff6d5986
Carlos Bentin <3669@holbertonschool.com>
2021-10-12 20:11:50 +0000

    add task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - print every other character
+ *
+ * @str: string to print
+ * 
+ *
+ * Return: none - void function
+ *
+ *
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar(10);
+}
