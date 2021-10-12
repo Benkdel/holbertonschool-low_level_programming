76cb9089552d60fdb5c0123631562e1956edfb06
Carlos Bentin <3669@holbertonschool.com>
2021-10-12 20:47:06 +0000

    add tastk 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * print_array - print n elements of an array
+ *
+ * @a: array
+ * @n: elements to print of array a
+ *
+ * Return: none - void function
+ *
+ *
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n - 2)
+		printf("%d, ", a[i++]);
+	printf("%d\n", a[++i]);
+}
