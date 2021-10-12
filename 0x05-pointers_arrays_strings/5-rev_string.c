cb24cc628351bbdb270811edd2bab992e321bf47
Carlos Bentin <3669@holbertonschool.com>
2021-10-12 20:03:55 +0000

    add task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rev_string - reverse a string
+ *
+ * @s: string to be reverse
+ *
+ *
+ * Return: none - void
+ *
+ *
+ */
+void rev_string(char *s)
+{
+	char temp;
+	int length = 0;
+	int b = 0;
+	int e;
+
+	while (s[length] != '\0')
+		length++;
+	e = length - 1;
+	length /= 2;
+	printf("%s\n", s);
+	while (length--)
+	{
+		temp = s[b];
+		s[b] = s[e];
+		s[e] = temp;
+		b++;
+		e--;
+	}
+}
