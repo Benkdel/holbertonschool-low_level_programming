f4cee5d5d5132aee0bcd25f70358afeb3479d1b3
Carlos Bentin <3669@holbertonschool.com>
2021-10-13 14:37:07 +0000

    fix: atoit task

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,27 +1,16 @@
-/*
- * isNumericChar - checks if char is numeric or negative sign
- *
- * @x: char to evaluate
- *
- * Return: 1 if x is numeric, otherwise 0
- *
- */
-int isNumericChar(char x)
-{
-	return (((x >= '0' && x <= '9') || x == '-') ? 1 : 0);
-}
 
 /*
  * _atoi - gets int from string
  *
  * @s: string containing the int
  *
- * Return: integer
+ * Return: integer found in string s
  *
  */
+
 int _atoi(char *s)
 {
-	int res = 0;
+	unsigned int res = 0;
 	int sign = 1;
 	int i;
 	int numPart = 0;
@@ -31,7 +20,7 @@ int _atoi(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (isNumericChar(s[i]))
+		if ((s[i] >= '0' && s[i] <= '9') || s[i] == '-')
 		{
 			if (s[i] == '-')
 				sign *= -1;
