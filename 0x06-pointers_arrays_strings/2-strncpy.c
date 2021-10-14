e764b7baec86dfd6c076d0c3d788b4f34c621718
Carlos Bentin <3669@holbertonschool.com>
2021-10-14 22:58:11 +0000

    add task 3

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,37 @@
+/**
+ * _strlen - returns length of string
+ *
+ * @s: string to be evaluated
+ *
+ * Return: integer of string length
+ *
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
+
+/**
+ * *_strncat - copy a string into another
+ *
+ * @dest: string dest
+ * @src: string src
+ * @n: number of bytes to get from dest
+ *
+ * Return: pointer to string dest
+ *
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+	return (dest);
+}
