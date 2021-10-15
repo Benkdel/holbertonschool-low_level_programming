381049feec5a0c681b2caad46aae6cce5f291e8f
Carlos Bentin <3669@holbertonschool.com>
2021-10-15 04:05:18 +0000

    add task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+
+/**
+ * *leet - encodes using leet code
+ *
+ * @s: string to be encoded
+ *
+ * Return: string encoded
+ *
+ */
+char *leet(char *s)
+{
+	int j;
+	int i = 0;
+	char mapping[5][2] = {"Aa", "Ee", "Oo", "Tt", "Ll"};
+	int numbers[] = {52, 51, 48, 55, 49};
+
+	while (s[i])
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (*(a + i) == mapping[j][0] || *(a + i) == mapping[j][1])
+				*(a + i) = numbers[j];
+		}
+		i++;
+	}
+}
