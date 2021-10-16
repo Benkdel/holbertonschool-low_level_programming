ec9a96e0648f9e99c59101d92418324902b1f4d2
Carlos Bentin <3669@holbertonschool.com>
2021-10-16 03:26:08 +0000

    adding file to complete later

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * *infinite_add - adds two numbers
+ *
+ * @n1: number to be added
+ * @n2: number to be added
+ * @r: buffer to store result
+ * @size_r: size of buffer
+ *
+ * Return: pointer to buffer r
+ *
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	return (r);
+}
