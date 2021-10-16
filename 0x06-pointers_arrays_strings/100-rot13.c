b1ffbfd8ed339787a0e10f8eee68415b01e28de6
Carlos Bentin <3669@holbertonschool.com>
2021-10-16 02:52:59 +0000

    add task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+
+/**
+ * *rot13 - encode text with rot13
+ *
+ * @s: string to encode
+ *
+ * Return: pointer to encoded string
+ *
+ */
+char *rot13(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		while ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
+		{
+			if ((s[i] > 'm' && s[i] <= 'z') || (s[i] > 'M' && s[i] <= 'Z'))
+			{
+				s[i] -= 13;
+				break;
+			}
+
+			s[i] += 13;
+			break;
+		}
+
+		i++;
+	}
+	return (s);
+}
