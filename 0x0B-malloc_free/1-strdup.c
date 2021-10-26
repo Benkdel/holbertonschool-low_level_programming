7bb36eac71abee7d2db0c3fca893cc6197de7485
Carlos Bentin <3669@holbertonschool.com>
2021-10-26 20:48:19 +0000

    adding task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *_strdup - returns a pointer to a newly allocated space in memory
+ *
+ * @str: array of chars to be copied
+ *
+ * Return: pointer to copy of str in new memory space
+ *
+ */
+char *_strdup(char *str)
+{
+	char *cpy;
+	unsigned int len = 0, i = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[i] != '\0')
+	{
+		len++;
+		i++;
+	}
+
+	cpy = malloc(len * sizeof(char));
+
+	if (cpy != NULL)
+	{
+		i = 0;
+		while (i < len)
+		{
+			cpy[i] = str[i];
+			i++;
+		}
+		return (cpy);
+	}
+	else
+		return (NULL);
+}
