7ee88b3298e804589944145db35eabbcd986f701
Carlos Bentin <3669@holbertonschool.com>
2021-10-27 02:26:45 +0000

    adding task 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,33 @@
+#include"main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * **alloc_grid - allocate a two dimensional array
+ *
+ * @width: columns of array
+ * @height: rows of array
+ *
+ * Return: pointer to a two dimensional array
+ *
+ */
+int **alloc_grid(int width, int height)
+{
+	int **arr;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	arr = malloc(height * sizeof(int));
+	for (i = 0; i < height; i++)
+		arr[i] = malloc(width * sizeof(int));
+
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			arr[i][j] = 0;
+	if (arr != NULL)
+		return (arr);
+	else
+		return (NULL);
+}
