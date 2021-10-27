e37c9a5284c7b41c29833d02e6c9e1285c09bb88
Carlos Bentin <3669@holbertonschool.com>
2021-10-27 16:53:30 +0000

    adding adv task 3

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * *infinite_add - adds two numbers
@@ -13,9 +14,9 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1, len2, l, m;
-	int i = 0;
-	int num1, num2, sum, count = 0, carry = 0;
+	int len1, len2, l;
+	int i = 0, j, m;
+	int num1, num2, sum, carry = 0;
 
 	while (n1[i] != '\0')
 	{
@@ -24,35 +25,47 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	}
 
 	i = 0;
-	while (n2[i++] != '\0')
+	while (n2[i] != '\0')
 	{
 		len2++;
 		i++;
 	}
 
 	l = (len1 > len2) ? len1 : len2;
-	i = l;
+
+	i = len1 - 1;
+	j = len2 - 1;
 
 	if (l + 1 > size_r)
 		return (0);
 
-        for (m = l - 1; m >=
+	for (m = l - 1; i >= 0 || j >= 0; --m)
 	{
-		num1 = (n1[len1] > 0) ? n1[len1] - '0' : 0;
-		num2 = (n2[len2] > 0) ? n2[len2] - '0' : 0;
+		num1 = (i >= 0) ? n1[i] - '0' : 0;
+		num2 = (j >= 0) ? n2[j] - '0' : 0;
 		sum = num1 + num2 + carry;
 		carry = 0;
 
 		if (sum > 9)
 		{
-			r[i] = sum % 10 + '0';
+			r[m] = (sum % 10) + '0';
 			carry = (sum - (sum % 10)) / 10;
 		}
 		else
-			r[i] = sum + '0';
-		i++;
-		count++;
+			r[m] = sum + '0';
+		--i;
+		--j;
 	}
-	r[l + 1] = '\0';
+
+	if (carry > 0)
+	{
+		r[l + 1] = '\0';
+		if (l + 2 > size_r)
+			return (0);
+		while (l-- >= 0)
+			r[l + 1] = r[l];
+		r[0] = carry + '0';
+	}
+
 	return (r);
 }
