6f24b01010b6e371fc0b9c9027ea5decf012f6eb
Carlos Bentin <3669@holbertonschool.com>
2021-10-27 02:54:05 +0000

    fix task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,7 +19,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	arr = malloc(height * sizeof(int));
+	arr = malloc(height * sizeof(int *));
 	for (i = 0; i < height; i++)
 		arr[i] = malloc(width * sizeof(int));
 
