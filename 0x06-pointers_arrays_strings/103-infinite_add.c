6a1eb79e7a6099180b3d64577d4e2aa78b270e23
Carlos Bentin <3669@holbertonschool.com>
2021-10-27 18:12:55 +0000

    fix adv task 3

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -14,50 +14,30 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1, len2, l;
-	int i = 0, j, m;
-	int num1, num2, sum, carry = 0;
+	int l, i = 0, j = 0, m, num1, num2, carry = 0;
 
 	while (n1[i] != '\0')
-	{
-		len1++;
 		i++;
-	}
+	while (n2[j] != '\0')
+		j++;
 
-	i = 0;
-	while (n2[i] != '\0')
-	{
-		len2++;
-		i++;
-	}
-
-	l = (len1 > len2) ? len1 : len2;
-
-	i = len1 - 1;
-	j = len2 - 1;
+	l = (i > j) ? i : j;
 
 	if (l + 1 > size_r)
 		return (0);
 
-	for (m = l - 1; i >= 0 || j >= 0; --m)
+	r[l] = '\0';
+	for (m = l - 1; m >= 0; m--)
 	{
+		i--;
+		j--;
 		num1 = (i >= 0) ? n1[i] - '0' : 0;
 		num2 = (j >= 0) ? n2[j] - '0' : 0;
-		sum = num1 + num2 + carry;
-		carry = 0;
 
-		if (sum > 9)
-		{
-			r[m] = (sum % 10) + '0';
-			carry = (sum - (sum % 10)) / 10;
-		}
-		else
-			r[m] = sum + '0';
-		--i;
-		--j;
+		r[m] = (num1 + num2 + carry) % 10 + '0';
+		carry = (num1 + num2 + carry) / 10;
 	}
-
-	if (carry > 0)
+	if (carry == 1)
 	{
 		r[l + 1] = '\0';
 		if (l + 2 > size_r)
