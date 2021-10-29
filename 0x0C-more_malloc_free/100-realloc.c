6e4e1b23fe54c9d2893ae0e18aeca1e4cfc90ef9
Carlos Bentin <3669@holbertonschool.com>
2021-10-29 02:59:44 +0000

    fix adv task 0

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,19 +17,21 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	char *tmp1, *tmp2;
 	unsigned int i = 0;
 
+	if (new_size <= 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
 	if (ptr == NULL)
 	{
 		ptr = malloc(new_size);
 		return (ptr);
 	}
-	if (new_size == old_size)
-		return (ptr);
-	if (new_size <= 0 && ptr != NULL)
-		free(ptr);
 	tmp2 = malloc(new_size);
 	if (tmp2 == NULL)
 	{
 		free(tmp2);
+		free(ptr);
 		return (NULL);
 	}
 	tmp1 = ptr;
@@ -49,7 +51,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	}
 	ptr = tmp2;
-	free(tmp2);
+	tmp2 = NULL;
 	free(tmp1);
+	free(tmp2);
 	return (ptr);
 }
