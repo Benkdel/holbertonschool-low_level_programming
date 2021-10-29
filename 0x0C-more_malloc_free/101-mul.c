de043b98812fdcd9eeb55406b96472e00a715709
Carlos Bentin <3669@holbertonschool.com>
2021-10-29 03:09:34 +0000

    todo 101

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry Point
+ *
+ * @argc: argument count
+ * @argv: arguments arrays
+ *
+ * Return: result of multiplying two numbers
+ *
+ */
+int main(int argc, char *argv)
+{
+	if (argc < 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	/* if argv[1] and argv[2] are not entirerly of digits, exit... */
+
+	/* finish later */
+
+	return (0);
+}
