55229546794eeac6b344a0e6dec30739eb37a0e8
Carlos Bentin <3669@holbertonschool.com>
2021-10-29 01:35:03 +0000

    fix task 1: NULL args

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,6 +17,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *new;
 	unsigned int len1 = 0, len2 = 0, i = 0, size = 0;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	while (s1[len1] != '\0')
 		len1++;
 	while (s2[len2] != '\0')
