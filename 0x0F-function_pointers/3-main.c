bebe037ebebf33ffd18dcc031c2114323f524e4a
Carlos Bentin <3669@holbertonschool.com>
2021-11-03 17:19:25 +0000

    pushing 3-main.c, was ignore by .gitignore

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,36 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: numb of arguments
+ * @argv: array of strings with args
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(int argc, char** argv)
+{
+	(void)argc;
+	int num1, num2, result;
+	char *op;
+	int (*fun)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op = argv[2];
+
+	fun = get_op_func(op);
+	result = fun(num1, num2);
+	printf("%d\n", result);
+
+	return (0);
+}
