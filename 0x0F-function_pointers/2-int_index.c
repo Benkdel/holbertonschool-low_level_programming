c5bbf1433c4ef149ad1560971585b7fd572335f4
Carlos Bentin <3669@holbertonschool.com>
2021-11-03 17:17:49 +0000

    adding validators to task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,9 +14,13 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int index = 0, i;
+	int index = -1, i;
 
 	if (size <= 0)
+		return (-1);
+	if (array == NULL)
+		return (-1);
+	if (cmp == NULL)
 		return (-1);
 
 	for (i = 0; i < size; i++)
