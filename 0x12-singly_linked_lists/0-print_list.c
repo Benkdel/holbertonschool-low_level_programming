38e04735c2f43ca4e9ed9d678bdfc9b880a07730
Carlos Bentin <cbentin12@gmail.com>
2021-11-11 21:28:57 -0500

    changing do while with while loop

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,14 +14,16 @@ size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
-	do {
+	while (h)
+	{
 		if (h->str == NULL)
 			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
+		
 		count++;
 		h = h->next;
-	} while (h != NULL);
-
+	}
+	
 	return (count);
 }
