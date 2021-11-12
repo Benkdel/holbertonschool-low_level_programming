d2ff601e54c014455b0f0da74571b11a7e1fb49c
Carlos Bentin <cbentin12@gmail.com>
2021-11-11 21:30:06 -0500

    fixing betty style

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -20,10 +20,10 @@ size_t print_list(const list_t *h)
 			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
-		
+	
 		count++;
 		h = h->next;
 	}
-	
+
 	return (count);
 }
