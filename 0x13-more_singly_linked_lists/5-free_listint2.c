addc519c0d5f31bb8e37d8d5faaf012422407a0d
Carlos Bentin <cbentin12@gmail.com>
2021-11-15 12:39:49 -0500

    Added Task 9 and Refactored Task 5 1) Implemented function to add node at index 2) Removed function from task 5. Was duplicate of task 4 3) Added testcases and updated hader file

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,23 +1,6 @@
 #include "lists.h"
 
 /**
- * recursive_free - free all elements of linked list
- *
- * @head: listint_t list to be freed
- *
- * Return: number of nodes in the list
- *
- */
-void recursive_free(listint_t *head)
-{
-	if (head == NULL)
-		return;
-
-	free_listint(head->next);
-	free(head);
-}
-
-/**
  * free_listint2 - free all elements of linked list
  *
  * @head: listint_t list to be freed
@@ -30,7 +13,7 @@ void free_listint2(listint_t **head)
 	if (head == NULL)
 		return;
 
-	recursive_free(*head);
+	free_listint(*head);
 
 	*head = NULL;
 }
---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * *insert_nodeint_at_index - inserts a new node at a given position
+ *
+ * @head: array of nodes
+ * @idx: index of list where new node should be added
+ * @n: value to add 
+ * Return: describe
+ *
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *newNode = malloc(sizeof(listint_t));
+	listint_t *prev, *next;
+
+	if (newNode == NULL || head == NULL)
+		return (NULL);
+
+	newNode->n = n;
+	prev = get_nodeint_at_index(*head, idx - 1);
+	next = get_nodeint_at_index(*head, idx);
+
+	if (prev != NULL && next != NULL)
+	{
+		prev->next = newNode;
+		newNode->next = next;
+		return (newNode);
+	}
+
+	free(newNode);
+	return (NULL);
+}
