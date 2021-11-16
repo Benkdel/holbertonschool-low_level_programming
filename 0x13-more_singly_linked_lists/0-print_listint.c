a6317a447eb83c51b7e9fffe5c25b60b93e8d6c0
Carlos Bentin <cbentin12@gmail.com>
2021-11-16 09:05:29 -0500

    removing comments from header file

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,13 +1,9 @@
-#include <stdio.h>
 #include "lists.h"
 
 /**
  * print_listint - prints all the elements of a listint_t list
- *
  * @h: listint_t list
- *
  * Return: number of nodes in the list
- *
  */
 size_t print_listint(const listint_t *h)
 {
