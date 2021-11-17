bcb92153ba8ea377c633791dc74c37d65df9e17d
Carlos Bentin <cbentin12@gmail.com>
2021-11-16 21:25:27 -0500

    fixing task 10:

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,90 +1,38 @@
 #include "lists.h"
-
 /**
- * *get_node2 - returns node at index
- *
- * @head: pointer to head of linked list
- * @index: index of node to retrieve
- *
- * Return: adress of node at index
- *
- */
-listint_t *get_node2(listint_t *head, unsigned int index)
-{
-	listint_t *node;
-	unsigned int n = 0;
-
-	if (head == NULL)
-		return (NULL);
-
-	node = head;
-
-	while (node)
-	{
-		if (n == index)
-			return (node);
-		n++;
-		node = node->next;
-	}
-	return (NULL);
-}
-
-/**
- * len_of_list - returns number of elements
- *
- * @h: len_of_list
- *
- * Return: number of nodes in the list
- *
- */
-size_t len_of_list(const listint_t *h)
-{
-	int nodes = 0;
-
-	if (h == NULL)
-		return (0);
-
-	nodes += len_of_list(h->next) + 1;
-
-	return (nodes);
-}
-
-/**
- * *delete_nodeint_at_index - deletes a new node at a given position
- *
- * @head: array of nodes
- * @index: index of list where new node should be added
- * Return: 1 if it succeded, -1 if it failed
- *
+ * delete_nodeint_at_index - inser new node
+ * @head: list
+ * @index: index
+ * Return: 1 if it succeeded, -1 if it failed
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *prev, *current;
-	unsigned int listLen;
+	unsigned int i = 1;
+	listint_t *prev_node, *next_node;
 
-	if (head == NULL)
-		return (-1);
-
-	listLen = (unsigned int)len_of_list(*head);
-
-	if (listLen == 0)
-		return (-1);
-
-	if (index == 0)
+	if (head != NULL)
 	{
-		current = (*head)->next;
-		*head = current;
-		return (1);
-	}
-
-	prev = get_node2(*head, index - 1);
-	current = get_node2(*head, index);
-
-	if (prev != NULL && current != NULL)
-	{
-		prev->next = current->next;
-		current->next = NULL;
-		free(current);
+		prev_node = *head;
+		while (prev_node != NULL && index > i)
+		{
+			prev_node = prev_node->next;
+			i++;
+		}
+		if (prev_node == NULL && index != i)
+		{
+			return (-1);
+		}
+		next_node = prev_node->next;
+		if (index == 0)
+		{
+			free(prev_node);
+			*head = next_node;
+		}
+		else
+		{
+			prev_node->next = next_node->next;
+			free(next_node);
+		}
 		return (1);
 	}
 	return (-1);
