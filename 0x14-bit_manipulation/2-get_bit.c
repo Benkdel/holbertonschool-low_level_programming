13f683f1451c2e52cc60a1e875c42686c886ab67
Carlos Bentin <cbentin12@gmail.com>
2021-11-18 21:23:14 -0500

    fix task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -13,12 +13,15 @@ int get_bit(unsigned long int n, unsigned int index)
 	if (index == 0)
 		return ((n >> 1) & 1);
 
-	while (n >> 1)
+	if (index < 64)
 	{
-		n = n >> 1;
-		if (pos == index - 1)
-			return (n & 1);
-		pos++;
+		while (n >> 1)
+		{
+			n = n >> 1;
+			if (pos == index - 1)
+				return (n & 1);
+			pos++;
+		}
 	}
 	return (-1);
 }
