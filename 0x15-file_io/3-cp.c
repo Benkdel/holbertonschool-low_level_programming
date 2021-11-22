3c30705f129902202e81767e89cf312e000dfde3
Carlos Bentin <cbentin12@gmail.com>
2021-11-22 13:12:35 -0500

    fix task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,6 +18,24 @@ int close_file(int fd)
 }
 
 /**
+ * create_buffer - Allocates 1024 bytes for a buffer
+ * @fd_src: The name of the file buffer is storing chars for
+ * Return: A pointer to the newly-allocated buffer
+ */
+char *create_buffer(char *fd_src)
+{
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * BUFFERSIZE);
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", fd_src);
+		exit(99);
+	}
+	return (buffer);
+}
+
+/**
  * main - copies contents of 1 file to another
  * @argc: number of parameters
  * @argv: array of string parameters
@@ -25,7 +43,7 @@ int close_file(int fd)
  */
 int main(int argc, char **argv)
 {
-	int fd_src, fd_dest, res = 0;
+	int fd_src, fd_dest, res = 0, w = 0;
 	char *buffer;
 
 	(void)argc;
@@ -35,30 +53,30 @@ int main(int argc, char **argv)
 		exit(97);
 	}
 	fd_src = open(argv[1], O_RDONLY);
-	if (fd_src == -1)
+	fd_dest = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd_src == -1 || fd_dest == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fd_dest = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
-	if (fd_dest == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		if (close_file(fd_src) == 100)
-			exit(100);
-		exit(99);
-	}
-	buffer = malloc(sizeof(char) * BUFFERSIZE);
-	if (buffer == NULL)
-	{
-		if (close_file(fd_dest) == 100 || close_file(fd_src) == 100)
-			exit(100);
-		return (-1);
-	}
+
+	buffer = create_buffer(argv[2]);
 	do {
 		res = read(fd_src, buffer, BUFFERSIZE);
-		write(fd_dest, buffer, res);
+		if (res == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		w = write(fd_dest, buffer, res);
+		if (w == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
 	} while (res > 0);
 	free(buffer);
+	close_file(fd_src);
+	close_file(fd_dest);
 	return (0);
 }
