670d44b39c1a2b4265d54f8bee52f7af20c7b4c5
Carlos Bentin <cbentin12@gmail.com>
2021-11-22 12:49:58 -0500

    Fix task 0 1) freeing buffer if write fails 2) changing output to STDOUT_FILENO

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,7 +15,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	/* Open file and get file descriptor */
-	fd = open(filename, 2);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 
@@ -34,11 +34,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 
 	/* write to standard output */
-	count = write(1, buffer, letters);
+	count = write(STDOUT_FILENO, buffer, letters);
 	if (count == -1)
 	{
-		return (0);
+		free(buffer);
 		close(fd);
+		return (0);
 	}
 	close(fd);
 
