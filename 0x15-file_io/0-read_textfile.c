0ea547efb4a3b22371bd50b4bd08b09fb5eb9c0b
Carlos Bentin <cbentin12@gmail.com>
2021-11-22 12:54:56 -0500

    fix read_textfile task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,14 +9,14 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buffer;
-	int count, fd;
+	int count, fd_o, fd_r;
 
 	if (filename == NULL)
 		return (0);
 
 	/* Open file and get file descriptor */
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	fd_o = open(filename, O_RDONLY);
+	if (fd_o == -1)
 		return (0);
 
 	/* allocate memory in buffer to read contents of fd */
@@ -25,23 +25,25 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	/* read contents of fd */
-	count = read(fd, buffer, letters);
-	if (count == 0)
+	fd_r = read(fd_o, buffer, letters);
+	if (fd_r == 0)
 	{
 		free(buffer);
-		close(fd);
+		close(fd_o);
 		return (0);
 	}
 
 	/* write to standard output */
-	count = write(STDOUT_FILENO, buffer, letters);
+	count = write(STDOUT_FILENO, buffer, fd_r);
 	if (count == -1)
 	{
 		free(buffer);
-		close(fd);
+		close(fd_r);
+		close(fd_o);
 		return (0);
 	}
-	close(fd);
+	close(fd_r);
+	close(fd_o);
 
 	return (count);
 }
