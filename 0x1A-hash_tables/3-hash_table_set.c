052cda60d4b0f78db659cdba51f91db572611990
Carlos Bentin <cbentin12@gmail.com>
2022-01-28 16:52:37 -0500

    adding key validator

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *head = NULL;
 	hash_node_t *node = NULL;
 
-	if (ht == NULL || key == NULL || value == NULL)
+	if (ht == NULL || key == NULL || value == NULL || *key == '\0')
 		return (0);
 
 	index = (hash_djb2((const unsigned char *)key) % ht->size);
