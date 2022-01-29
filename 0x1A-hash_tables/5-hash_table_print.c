d4b4345d3a52688e10cfeee0f28c21d37deba1d3
Carlos Bentin <cbentin12@gmail.com>
2022-01-28 19:00:37 -0500

    adding method to print hasth table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,11 +1,30 @@
 #include "hash_tables.h"
 
 /**
- * @brief
- *
- * @return int
+ * hash_table_print - prints a hash table
+ * @ht: pointer to a hash table
+ * Return: none - void function
  */
-int pass2()
+void hash_table_print(const hash_table_t *ht)
 {
-	return (0);
+	hash_node_t *current = NULL;
+	unsigned long int index;
+	unsigned long int counter = 0;
+
+	if (ht == NULL)
+		return;
+	printf("{");
+	for (index = 0; index < ht->size; index++)
+	{
+		if (counter != 0 && ht->array[index] != NULL)
+			printf(", ");
+		current = ht->array[index];
+		while (current != NULL)
+		{
+			printf("'%s': '%s'", current->key, current->value);
+			current = current->next;
+			counter++;
+		}
+	}
+	printf("}\n");
 }
