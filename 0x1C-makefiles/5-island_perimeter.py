336f5caef561a5637df45f1e97b48336aa693cb6
Carlos Bentin <cbentin12@gmail.com>
2022-03-18 02:18:52 -0500

    Adding remaining tasks

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Technical interview preparation.
+"""
+
+
+def island_perimeter(grid):
+    """
+    Function: Perimeter of the island
+        Args:
+            grid(list): List of list of integers
+        Returns:
+            Returns the perimeter of the island described in grid.
+    """
+    land = 1
+    water = 0
+    rows = len(grid)
+    result = 0
+    for x in range(0, rows):
+        for y in range(0, len(grid[x])):
+            if grid[x][y] == land:
+                if (y + 1) == len(grid[x]) or grid[x][y + 1] == water:
+                    result += 1
+                if (y - 1) == -1 or grid[x][y - 1] == water:
+                    result += 1
+                if (x + 1) == rows or grid[x + 1][y] == water:
+                    result += 1
+                if (x - 1) == -1 or grid[x - 1][y] == water:
+                    result += 1
+    return result
