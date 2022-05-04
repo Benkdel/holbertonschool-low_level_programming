2cfcc06723ee0840c78ed9bb47ae261feecbee3e
Carlos Bentin <cbentin12@gmail.com>
2022-05-03 19:41:50 -0500

    remaining tasks

---
+++ 0x1E-search_algorithms/103-exponential.c
@@ -0,0 +1,13 @@
+#include "search_algos.h"
+
+/**
+ * exponential_search - looks for a value in an array
+ * @array: pointer to first element of array
+ * @size: number of elements in the array
+ * @value: target
+ * Return: idx of the target if found, -1 otherwise
+ */
+int exponential_search(int *array, size_t size, int value)
+{
+	return (0);
+}
